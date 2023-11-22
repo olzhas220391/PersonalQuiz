5ec671cd7be38a17daac9ffc5ce96aca17660ef5
Learn Swift <idev80299@gmail.com>
2023-11-22 21:07:21 +0600

    Доработка

--- PersonalQuiz/ViewControllers/ViewController.swift
+++ PersonalQuiz/ViewControllers/ViewController.swift
@@ -6,9 +6,9 @@
 //
 
 import UIKit
-
+// У нас во вьюконтроллере должны быть данные, для того чтобы мы их могли отобразить. Создадим приватный экземпляр модели данных private let questions.
 class ViewController: UIViewController {
-    private let questions = Question.getQuestions()
+    private let questions = Question.getQuestions() // Здесь у нас появился массив благодаря методу static func getQuestions() -> [Question] прописанный в файле Question.
     
     override func viewDidLoad() {
         super.viewDidLoad()
