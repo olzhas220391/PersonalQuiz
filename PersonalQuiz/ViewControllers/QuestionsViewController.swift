453b32e337242422d2a03e0dcbcd7e19bce29df0
Learn Swift <idev80299@gmail.com>
2023-11-23 09:12:44 +0600

    continuation

--- PersonalQuiz/ViewControllers/QuestionsViewController.swift
+++ PersonalQuiz/ViewControllers/QuestionsViewController.swift
@@ -7,7 +7,11 @@
 
 import UIKit
 // У нас во вьюконтроллере должны быть данные, для того чтобы мы их могли отобразить. Создадим приватный экземпляр модели данных private let questions.
-class ViewController: UIViewController {
+class QuestionsViewController: UIViewController {
+    
+    @IBOutlet var questionProgressView: UIProgressView!
+    @IBOutlet var questionLabelView: UILabel!
+    
     private let questions = Question.getQuestions() // Здесь у нас появился массив благодаря методу static func getQuestions() -> [Question] прописанный в файле Question.
     
     override func viewDidLoad() {
---
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ResultViewController.swift
+//  PersonalQuiz
+//
+//  Created by Назар on 23.11.2023.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+
+}
